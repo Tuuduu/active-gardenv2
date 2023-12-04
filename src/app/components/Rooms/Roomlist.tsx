5ffa1ee5ea7c7f171685e8e5b8883460f1992e70
Tuuduu <munhtogtohganbat@yahoo.com>
2023-12-04 18:29:15 +0800

    add Roomlist

--- src/app/components/Rooms/Roomlist.tsx
+++ src/app/components/Rooms/Roomlist.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import aroom01 from '@/images/RoomsPage/A ROOM/AVG-ALL-ROOMS_38.63-3D-HALF-PLAN.jpg'
+import { Image } from 'antd';
 
 export default function Roomlist() {
+    const temp01 = [
+        {
+            image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        },
+        {
+            image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        }
+    ]
+
+
     return (
-        <div>Roomlist</div>
+        <div>
+            {temp01.map((item) => (
+                <Image
+                    width={200}
+                    src={item.image}
+                />
+            ))
+            }
+        </div>
+
     )
 }
--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Roomlist from '../components/Rooms/Roomlist'
 export default function rooms() {
   return (
-    <div className='w-full py-10'>
-      <div className="mb-6 px-10 ">
+    <div className='w-full p-10'>
+      <div className="mb-6">
         <h1 className="text-[34px] font-[400] uppercase">ӨРӨӨНИЙ СОНГОЛТ</h1>
         <h2 style={{
           content: '',
@@ -13,6 +14,7 @@ export default function rooms() {
           margin: '15px 0',
         }} > </h2>
       </div>
+      <Roomlist />
     </div>
   )
 }
