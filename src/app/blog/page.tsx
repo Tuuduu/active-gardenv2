66ffc2e245c5e5c958ebe5847b978fec0db0aeea
Tuuduu <munhtogtohganbat@yahoo.com>
2023-12-17 17:16:07 +0800

    й

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
+import fix from '@/images/BlogImages/giphy.gif'
+
 export default function blog() {
   return (
-    <div className='w-full bg-red-200'>
-      this is blog page
+    <div className='w-full h-screen flex items-center justify-center'>
+      <Image
+        alt='server error'
+        src={fix}
+        quality={100}
+
+        className='w-auto h-auto '
+      />
     </div>
   )
 }
--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
+import fix from '@/images/BlogImages/giphy.gif'
+
 export default function contact() {
   return (
-    <div className='w-full bg-red-200'>
-      this is contact page
+    <div className='w-full h-screen flex items-center justify-center'>
+      <Image
+        alt='server error'
+        src={fix}
+        quality={100}
+
+        className='w-auto h-auto '
+      />
     </div>
   )
 }
