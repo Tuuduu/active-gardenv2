b8d3747d8c83ea0cc39bcbeea110e00f02a28910
Tuuduu <munhtogtohganbat@yahoo.com>
2024-01-30 18:29:01 +0800

    11

--- src/app/components/FarIndecatorPage/FarIndecatorPageAnimation.tsx
+++ src/app/components/FarIndecatorPage/FarIndecatorPageAnimation.tsx
@@ -23,33 +23,26 @@ export default function FarIndecatorPageAnimation() {
 
 
     return (
-        <div className='w-full h-auto flex flex-row'>
-            <div>
+        <div className='w-full h-auto flex flex-row px-14'>
+            <div className='w-full h-auto flex flex-row'>
                 <h2 style={{
                     content: '',
                     display: 'block',
                     width: '100px',
                     height: '3px',
                     background: '#c19c2e',
-                    margin: '15px 0',
-                }} > </h2>
-                <h1
-                    className='dom-attribute-demo input'>
-                    0
-                </h1>
-                <div className='w-10 h-10 bg-green-200 div'>
-                    1
+                    margin: '80px 0',
+                }}
+                > </h2>
+                <div className='flex flex-col'>
+                    <h1 className='text-[18px] font-bold uppercase'>НЯГТАРШИЛ</h1>
+                    <h1 className='text-[47px] font-bold uppercase'>БАГА</h1>
                 </div>
-            </div>
-            <div>
-                <animated.div
-                    className={'w-10 h-10 bg-green-200 '}
-                    onClick={handleClick}
-                    style={{
-                        ...springs,
-                    }}>
-
-                </animated.div>
+                <div className='bg-red-200 flex'>
+                    <h1 className='text-[94px] font-bold text-[#C19C2E]'>
+                        1.2 FAR
+                    </h1>
+                </div>
             </div>
         </div>
     )
